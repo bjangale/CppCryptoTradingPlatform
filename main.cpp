ad4bb799bea196df4e19eaf2d850f3ae14160412
bnjangale <bnj1306@gmail.com>
2024-06-11 13:22:01 +0530

    Refactored the code into a set of function, each with clearly defined purpose. Seperation of concerns

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 
 void printMenu()
 {
-    std::cout<<"============================"<<std::endl;
+    std::cout << "============================" << std::endl;
     std::cout << "1: Print help" << std::endl;
     std::cout << "2: Print exchange stats" << std::endl;
     std::cout << "3: Place an ask" << std::endl;
     std::cout << "4: Place a bid" << std::endl;
     std::cout << "5: Print wallet" << std::endl;
     std::cout << "6: Continue" << std::endl;
-    std::cout<<"============================"<<std::endl;
+    std::cout << "============================" << std::endl;
 }
 
 int getUserInput()
@@ -20,28 +20,58 @@ int getUserInput()
     return userOption;
 }
 
+void printHelp()
+{
+    std::cout << "Help - choose options from the menu" << std::endl;
+    std::cout << "and follow the screen instructions." << std::endl;
+}
+
+void printMarketStats()
+{
+    std::cout << "Display exchange matching engine statistics" << std::endl;
+}
+
+void enterOffer()
+{
+    std::cout << "Make an offer - enter amount" << std::endl;
+}
+
+void enterBid()
+{
+    std::cout << "Make an bid - enter amount" << std::endl;
+}
+
+void printWallet()
+{
+    std::cout << "Your wallet is empty." << std::endl;
+}
+
+void gotoNextTimeFrame()
+{
+    std::cout << "Going to next time frame" << std::endl;
+}
+
 void processOption(int userOption)
 {
     switch (userOption)
     {
     case 1:
-        std::cout << "Help - choose options from the menu" << std::endl;
-        std::cout << "and follow the screen instructions." << std::endl;
+        printHelp();
         break;
     case 2:
-        std::cout << "Display exchange matching engine statistics" << std::endl;
+        printMarketStats();
         break;
     case 3:
-        std::cout << "Make an offer - enter amount" << std::endl;
+        enterOffer();
         break;
     case 4:
-        std::cout << "Make an bid - enter amount" << std::endl;
+        enterBid();
         break;
     case 5:
-        std::cout << "Your wallet is empty." << std::endl;
+        printWallet();
         break;
     case 6:
-        std::cout << "Going to next time frame" << std::endl;
+        gotoNextTimeFrame();
         break;
     default:
         std::cout << "Invalid option choose. Choose 1-6" << std::endl;
@@ -54,11 +84,9 @@ int main()
     while (1)
     {
         printMenu();
-
         int userOption = getUserInput();
-
         processOption(userOption);
     }
+
     return 0;
 }
-    
